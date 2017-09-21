6f39bbb4e469813605560f3ac6f9b02ad8ad1149
Ben Rudolph <rudolphben@gmail.com>
2017-09-21 15:26:32 -0700

    leave trends out for now

--- app/javascript/global/components/main_header.jsx
+++ app/javascript/global/components/main_header.jsx
@@ -11,9 +11,6 @@ class MainHeader extends React.Component {
             <nav className="navbar navbar-light bg-faded">
                 <a className="navbar-brand" href="/">ThingsOnReddit</a>
                 <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="/trends/">Trends</a>
-                  </li>
                 </ul>
                 <ThingFilters />
             </nav>
