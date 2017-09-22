4b306f495ecb24e5e783d1126c589caa2ad6d8df
Ben Rudolph <rudolphben@gmail.com>
2017-09-21 17:08:40 -0700

    replace their links with my links

--- app/javascript/comments/components/show.jsx
+++ app/javascript/comments/components/show.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import $ from 'jquery/dist/jquery'
+import _ from 'underscore/underscore'
 
 import { connect } from 'react-redux'
 import Showdown from 'showdown/dist/showdown'
@@ -9,6 +11,7 @@ class ThingSnippet extends React.Component {
     getLink() {
         return this.props.thing.amazon_link + '?tag=benrudolph-20'
     }
+
     render() {
         const thing = this.props.thing
         return (
@@ -21,9 +24,41 @@ class ThingSnippet extends React.Component {
 }
 
 class CommentShow extends React.Component {
-    render() {
+
+    decodeEntities(encodedString) {
+        // https://stackoverflow.com/a/31350391/835696
+        let textArea = document.createElement('textarea');
+        textArea.innerHTML = encodedString;
+        return textArea.value;
+    }
+
+    renderBody() {
         const comment = this.props.comment
         const converter = new Showdown.Converter()
+        const body = converter.makeHtml(comment.body)
+        const $body = $('<div>' + body + '</div>')
+        const self = this
+
+        // First let's see if we can replace any of the links
+        $body.find('a').each(function(idx) {
+            let $a = $(this)
+            const href = $a.attr('href')
+            comment.things.forEach((thing) => {
+                // Sometimes there is a trailing paren on the link. This trims it
+                console.log(thing.original_link)
+                const cleanedHref = self.decodeEntities(thing.original_link.slice(0, -3))
+                console.log(cleanedHref)
+                if (href.includes(cleanedHref)) {
+                    $a.attr('href', thing.amazon_link + '?tag=benrudolph-20')
+                }
+            })
+        })
+
+        return $body.html()
+    }
+
+    render() {
+        const comment = this.props.comment
         const thingSnippets = comment.things.map((thing, i) => {
             return <ThingSnippet key={i} thing={thing} />
         })
@@ -32,7 +67,7 @@ class CommentShow extends React.Component {
                 <div className="comment-body">
                     <h2 className="mb-3">{'/r/' + comment.subreddit}</h2>
                     <blockquote className="blockquote">
-                        <p className="comment-text light" dangerouslySetInnerHTML={{ __html: converter.makeHtml(comment.body) }} >
+                        <p className="comment-text light" dangerouslySetInnerHTML={{ __html: this.renderBody() }} >
                         </p>
                         <footer className="blockquote-footer"><small className="text-muted">{comment.author}</small></footer>
                     </blockquote>
