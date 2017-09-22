8ff53d2b7660e4464abb800d6c6f351265df4540
Ben Rudolph <rudolphben@gmail.com>
2017-09-21 20:01:11 -0700

    connect to RDS

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'will_paginate', '~> 3.1.0'
 gem 'unicorn'
 gem 'upsert'
 gem 'groupdate'
+gem 'pg'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170921195949) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "score"
