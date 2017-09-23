5811402442601024a836d4cc2b2866d5e7d4b24b
Ben Rudolph <rudolphben@gmail.com>
2017-09-23 09:36:12 -0700

    new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "personal", user: "deploy", roles: %w{app db web}
+server "p2", user: "deploy", roles: %w{app db web}
 
 
 # role-based syntax
